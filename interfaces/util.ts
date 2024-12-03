6b493ce373b1bac6df457a83ffe34b71288793a7
zimoluo <zimo_luo@outlook.com>
2024-12-03 15:48:11 -0500

    you come back here

--- interfaces/util.ts
+++ interfaces/util.ts
@@ -1,1 +1,11 @@
 type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+interface ImageIconProps {
+  color?: HexColor | null;
+  className?: string;
+  height?: number;
+  width?: number;
+  isLight?: boolean;
+  isSaturated?: boolean;
+  strokeWidth?: number;
+}
