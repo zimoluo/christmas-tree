61622fd1dfec7f89eec7efb0bd56f9c807765cc0
zimoluo <zimo_luo@outlook.com>
2025-12-05 12:41:47 -0500

    big button rework

--- components/widgets/PopUpDisplay.tsx
+++ components/widgets/PopUpDisplay.tsx
@@ -60,15 +60,17 @@ export default function PopUpDisplay({
           </PopUpActionProvider>
         </div>
         {hasUtilityButton && (
-          <button
-            className={`absolute top-3 group right-3 z-70 flex items-center justify-center gap-5 ${popUpStyle.onlyChildRule} py-2.5 rounded-full bg-neutral-600/30 backdrop-blur-sm outline outline-1 outline-neutral-200/15`}
-            onClick={closeThisPopUpIfLast}
-          >
-            <CrossIcon
-              color="#efefef"
-              className="h-5 w-auto opacity-80 mix-blend-plus-lighter transition-transform duration-300 group-hover:scale-105"
-            />
-          </button>
+          <div className="absolute top-3 right-3 z-70 rounded-full">
+            <button
+              className={`group flex items-center justify-center p-2 rounded-full bg-neutral-600/30 backdrop-blur-sm border-reflect-light ${popUpStyle.specialReflect}`}
+              onClick={closeThisPopUpIfLast}
+            >
+              <CrossIcon
+                color="#efefef"
+                className="h-4 w-4 opacity-80 mix-blend-plus-lighter transition-transform duration-300 group-hover:scale-105"
+              />
+            </button>
+          </div>
         )}
       </div>
     </>
