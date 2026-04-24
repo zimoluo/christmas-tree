3045a23cd30d6167a1162eda72d3dbeb68f672e2
zimoluo <zimo_luo@outlook.com>
2026-04-24 12:10:06 -0400

    shadow!!

--- components/mainPage/menu/settings/ThemePickerButton.tsx
+++ components/mainPage/menu/settings/ThemePickerButton.tsx
@@ -31,7 +31,7 @@ export default function ThemePickerButton({
   return (
     <button
       key={theme}
-      className={`${themePickerStyle.ring} transition-colors duration-300 ease-in-out relative rounded-full group w-12 aspect-square h-12`}
+      className={`${themePickerStyle.ring} transition-colors duration-300 ease-in-out shadow-sm relative rounded-full group w-12 aspect-square h-12`}
       onClick={handleThemeChange}
     >
       <div
